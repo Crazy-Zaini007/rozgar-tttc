f0ce7181f70c4fb558a1dcc0800d76bee13aa248
codewithMERN <88480039+Crazy-Zaini007@users.noreply.github.com>
2024-03-05 04:14:23 -0700

    adding navigate to Login Hook

--- frontend/src/hooks/userHooks/LoginHook.js
+++ frontend/src/hooks/userHooks/LoginHook.js
@@ -1,13 +1,15 @@
 import {useState} from 'react'
 import { useAuthContext } from "./UserAuthHook";
+import {useNavigate} from 'react-router-dom'
 
 export default function LoginHook() {
     const {dispatch}=useAuthContext()
-
     const [isLoading,setLoading]=useState(false)
     const [error,setError]=useState(null)
     const [success,setSuccess]=useState(null)
     const[emptyFields,setEmptyFields]=useState([])
+
+    const navigate=useNavigate()
     
     // fetching api end-point to register a new user
     const userLogin=async(userName,password)=>{
@@ -35,6 +37,7 @@ export default function LoginHook() {
                     localStorage.setItem('user', JSON.stringify(json))
 
                         dispatch({type: 'USER_LOGIN', payload:json})
+                         navigate('/rozgar/dashboard')
                    
                     }, 1500);
                     setLoading(false)
