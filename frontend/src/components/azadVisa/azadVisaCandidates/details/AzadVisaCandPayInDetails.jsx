b40893743c84ccde13926f5a8c98b19492741347
codewithMERN <88480039+Crazy-Zaini007@users.noreply.github.com>
2024-03-09 13:21:40 -0700

    Rmeoving Duplicates Keys from Frontend fies

--- frontend/src/components/azadVisa/azadVisaCandidates/details/AzadVisaCandPayInDetails.jsx
+++ frontend/src/components/azadVisa/azadVisaCandidates/details/AzadVisaCandPayInDetails.jsx
@@ -251,7 +251,7 @@ export default function AzadVisaCandPaymentInDetails() {
           'Content-Type': 'application/json',
           "Authorization": `Bearer ${user.token}`,
         },
-        body: JSON.stringify({ name: editedEntry1.supplierName,pp_No:editedEntry1.pp_No,entry_Mode:editedEntry1.entry_Mode,company:editedEntry1.company,country:editedEntry1.country,trade:editedEntry1.trade,final_Status:editedEntry1.final_Status,flight_Date:editedEntry1.flight_Date,flight_Date:editedEntry1.flight_Date, total_Payment_In: editedEntry1.total_Payment_In, total_Cash_Out: editedEntry1.total_Cash_Out, total_Visa_Price_In_Curr: editedEntry1.total_Payment_In_Curr, open: editedEntry1.open, close: editedEntry1.close  })
+        body: JSON.stringify({ name: editedEntry1.supplierName,pp_No:editedEntry1.pp_No,entry_Mode:editedEntry1.entry_Mode,company:editedEntry1.company,country:editedEntry1.country,trade:editedEntry1.trade,final_Status:editedEntry1.final_Status,flight_Date:editedEntry1.flight_Date, total_Payment_In: editedEntry1.total_Payment_In, total_Cash_Out: editedEntry1.total_Cash_Out, total_Visa_Price_In_Curr: editedEntry1.total_Payment_In_Curr, open: editedEntry1.open, close: editedEntry1.close  })
       })
 
       const json = await response.json()
