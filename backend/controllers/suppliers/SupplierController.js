a0daf0d069000eb610c9999b8e1737458c9d7f90
codewithMERN <88480039+Crazy-Zaini007@users.noreply.github.com>
2024-06-12 17:54:50 +0500

    Updating Suppliers Controller

--- backend/controllers/suppliers/SupplierController.js
+++ backend/controllers/suppliers/SupplierController.js
@@ -3532,7 +3532,7 @@ const changePaymentOutStatus = async (req, res) => {
   try {
       const userId = req.user._id;
       const user = await User.findById(userId);
-      const{supplierName,newStatus}=req.body
+      const{supplierName,newStatus,multipleIds }=req.body
     
       if (!user) {
           return res.status(404).json({ message: "User not found" });
